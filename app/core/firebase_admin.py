1263458d4757c1a9371d75f887f10251b9fb2d9e
Aditya Raj <aditya26raj01@gmail.com>
2025-06-17 01:34:48 +0530

    Refactor Firebase admin initialization to use structured environment variables for credential management, improving security and maintainability.

--- app/core/firebase_admin.py
+++ app/core/firebase_admin.py
@@ -10,8 +10,22 @@ import base64
 # Check if any Firebase app instances are already initialized
 if not firebase_admin._apps:
     # Load Firebase credentials from environment variable
-    encoded: str = os.getenv("FIREBASE_ADMIN_CREDENTIALS_BASE64") or ""
-    decoded = base64.b64decode(encoded).decode("utf-8")
-    cred = credentials.Certificate(json.loads(decoded))
+    private_key = os.getenv("FIREBASE_PRIVATE_KEY", "").replace("\\n", "\n")
+
+    firebase_cred_dict = {
+        "type": os.getenv("FIREBASE_TYPE"),
+        "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+        "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+        "private_key": private_key,
+        "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+        "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+        "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+        "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+        "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+        "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL"),
+        "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN"),
+    }
+
+    cred = credentials.Certificate(firebase_cred_dict)
     # Initialize the Firebase app with the loaded credentials
     firebase_admin.initialize_app(cred)
