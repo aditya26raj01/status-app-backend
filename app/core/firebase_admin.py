a35b4444e263a67052457454ec1d1fa7fec814cd
Aditya Raj <aditya26raj01@gmail.com>
2025-06-17 10:20:40 +0530

    Refactor Firebase admin initialization to load credentials from a JSON environment variable, enhancing security and simplifying credential management.

--- app/core/firebase_admin.py
+++ app/core/firebase_admin.py
@@ -5,27 +5,15 @@ import firebase_admin
 from firebase_admin import credentials
 import os
 import json
-import base64
+from dotenv import load_dotenv
+
+load_dotenv()
+
+firebase_json = os.getenv("FIREBASE_CREDENTIALS_JSON")
 
 # Check if any Firebase app instances are already initialized
 if not firebase_admin._apps:
     # Load Firebase credentials from environment variable
-    private_key = os.getenv("FIREBASE_PRIVATE_KEY", "").replace("\\n", "\n")
-
-    firebase_cred_dict = {
-        "type": os.getenv("FIREBASE_TYPE"),
-        "project_id": os.getenv("FIREBASE_PROJECT_ID"),
-        "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-        "private_key": private_key,
-        "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
-        "client_id": os.getenv("FIREBASE_CLIENT_ID"),
-        "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
-        "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
-        "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
-        "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL"),
-        "universe_domain": os.getenv("FIREBASE_UNIVERSE_DOMAIN"),
-    }
-
-    cred = credentials.Certificate(firebase_cred_dict)
+    cred = credentials.Certificate(json.loads(firebase_json or ""))
     # Initialize the Firebase app with the loaded credentials
     firebase_admin.initialize_app(cred)
